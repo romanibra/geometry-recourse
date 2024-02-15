a0f9e834168d739e56319c1351875e151eba5000
romanibra <romanman242@gmail.com>
2024-02-15 23:29:05 +0600

    rectangle task added

--- scripts/rectangle.js
+++ scripts/rectangle.js
@@ -1,3 +1,22 @@
 function rectangleArea(){
-    console.log('rectangle react area')
+    const lengthInput=document.getElementById('rectangle-length')
+    const lengthText=lengthInput.value
+    const length=parseFloat(lengthText)
+    console.log(length)
+    
+
+    const widthInput=document.getElementById('rectangle-width');
+    const widthText=widthInput.value;
+    const width=parseFloat(widthText);
+    console.log(width);
+
+    // calculate value
+
+    const area=length*width;
+    console.log('rectangle area value is', area)
+
+    // display value
+
+    const rectangleAreaSpan=document.getElementById('rectangle-Area')
+    rectangleAreaSpan.innerText=area
 }
